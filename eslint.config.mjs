561c330d90b40326b954b6b9feced79b8adf2822
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-06-13 10:40:05 -0700

    Update configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,55 +1,17 @@
-import simpleImportSort from "eslint-plugin-simple-import-sort";
 import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-// module.exports = {
-//     env: {
-//         browser: true,
-//         es2021: true,
-//         webextensions: true,
-//     },
-//     extends: ['eslint:recommended', 'google', 'plugin:prettier/recommended'],
-//     overrides: [
-//         {
-//             env: {
-//                 node: true,
-//             },
-//             files: ['.eslintrc.{js,cjs}'],
-//             parserOptions: {
-//                 sourceType: 'script',
-//             },
-//         },
-//     ],
-//     parserOptions: {
-//         ecmaVersion: 'latest',
-//         sourceType: 'module',
-//     },
-//     plugins: ['simple-import-sort'],
-//     rules: {
-//         'prettier/prettier': 2,
-//         'simple-import-sort/imports': 'error',
-//         'simple-import-sort/exports': 'error',
-//     },
-// };
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
-    ...compat.extends("eslint:recommended", "google", "plugin:prettier/recommended"),
+    // Recommended config applied to all files
+    js.configs.recommended,
     {
-        plugins: {
-            // "simple-import-sort": simpleImportSort,
-        },
-
+        ignores: [
+            "build/**",
+            "dist/**",
+            "node_modules/**"
+        ],
         languageOptions: {
             globals: {
                 ...globals.browser,
@@ -58,24 +20,8 @@ export default [
 
             ecmaVersion: "latest",
             sourceType: "module",
-        },
-
-        rules: {
-            // "prettier/prettier": 2,
-            // "simple-import-sort/imports": "error",
-            // "simple-import-sort/exports": "error",
-        },
+        }
     },
-    {
-        files: ["**/.eslintrc.{js,cjs}"],
-
-        languageOptions: {
-            globals: {
-                ...globals.node,
-            },
-
-            ecmaVersion: 5,
-            sourceType: "commonjs",
-        },
-    },
+    eslintPluginPrettierRecommended,
+    eslintConfigPrettier
 ];
